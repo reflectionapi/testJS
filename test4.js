247950873e4459679902b2026f9f123e91398516
Artem Fadeev <reflectionapi@gmail.com>
2022-03-24 14:02:18 +0300

    Add files via upload

---
+++ test4.js
@@ -0,0 +1,19 @@
+/* 
+invertCase.js
+Реализуйте и экспортируйте по умолчанию функцию invertCase, которая меняет в строке регистр каждой буквы на противоположный.
+
+invertCase('Hello, World!'); // hELLO, wORLD!
+invertCase('I loVe JS');     // i LOvE js
+Подсказки
+Чтобы узнать длину строки, используйте свойство length:
+
+'welcome'.length; // 7
+Чтобы получить строку (или отдельный символ) в верхнем регистре, используйте метод toUpperCase:
+
+'foo'.toUpperCase(); // 'FOO';
+'f'.toUpperCase();   // 'F';
+Чтобы получить строку (или отдельный символ) в нижнем регистре, используйте метод toLowerCase:
+
+'BAR'.toLowerCase(); // 'bar';
+'b'.toLowerCase();   // 'b';
+ */
